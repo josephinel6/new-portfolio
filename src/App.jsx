15c9b5643bc7dc27479777247ecc866280783937
Josephine Li <josephinemouse@gmail.com>
2024-02-01 08:41:07 -0800

    add discord link

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,9 @@ function App() {
         <h1 id="name"> Josephine</h1>
         <h3> High school sophomore</h3>
         <h3> WA </h3>
-        <FontAwesomeIcon className="social" id="github" icon={faGithub} href="github.com" onClick={() => location.href = "https://www.github.com/josephinel6"} />
+        <FontAwesomeIcon className="social" id="github" icon={faGithub} onClick={() => location.href = "https://www.github.com/josephinel6"} />
         <FontAwesomeIcon className="social" icon={faEnvelope} href="mailto:mailjosephinel@gmail.com" onClick={() => location.href = "mailto:mailjosephinel@gmail.com"} />
+        <FontAwesomeIcon className="social" icon={faDiscord} onClick={() => location.href = "https://discordapp.com/users/886433195904602112"} />
 
       </div>
     </>
