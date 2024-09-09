e99d5405797fa91bf2e85fadf8965abbcc4f4de6
Josephine L. <josephinemouse@gmail.com>
2024-09-08 19:03:05 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <div id="card">
         <img src="/assets/profile-cropped.jpg" id="mainphoto" />
         <h1 id="name"> Josephine</h1>
-        <h3> High school sophomore</h3>
+        {/* <h3> High school sophomore</h3>  */}
         <h3> WA </h3>
         <FontAwesomeIcon className="social" id="github" icon={faGithub} onClick={() => location.href = "https://www.github.com/josephinel6"} />
         <FontAwesomeIcon className="social" icon={faEnvelope} href="mailto:mailjosephinel@gmail.com" onClick={() => location.href = "mailto:mailjosephinel@gmail.com"} />
